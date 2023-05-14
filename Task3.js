b54dd423099170c129c10d1e09b68b73204ee5f0
AmanSingh-02 <133587871+AmanSingh-02@users.noreply.github.com>
2023-05-14 20:53:35 +0530

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,44 @@
+function addList(e)
+
+{
+    const input = document.querySelector('input');
+    const btn = document.querySelector('.addTask > button');
+    const notCompleted = document.querySelector('.notCompleted');
+    const Completed = document.querySelector('.Completed');
+    const newLi = document.createElement('li');
+    const checkBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
+    checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+    delBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+    if(input.value !=='')
+
+	{
+
+		newLi.textContent = input.value;
+		input.value = '';
+		notCompleted.appendChild(newLi);
+		newLi.appendChild(checkBtn);
+		newLi.appendChild(delBtn);
+
+	}
+
+	checkBtn.addEventListener('click', function()
+
+	{
+
+		const parent = this.parentNode;
+		parent.remove();
+		Completed.appendChild(parent);
+		checkBtn.style.display = 'none';
+
+	});
+
+	delBtn.addEventListener('click', function()
+
+	{
+		const parent = this.parentNode;
+		parent.remove();
+	});
+
+}
